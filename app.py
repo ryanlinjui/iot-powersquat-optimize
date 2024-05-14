48d07599d86208a02f482c8f0cd19cb9e44ec596
RyanLin <ryanlinjui@gmail.com>
2024-05-15 01:36:52 +0800

    feat: flask app init

--- app.py
+++ app.py
@@ -7,11 +7,27 @@ from utils import (
     line_webhook
 )
 import os
+import logging
+import shutil
+
 import routes
 from routes.iot.receive import iot_receive_app
 
+logging.basicConfig(
+    level = logging.DEBUG,
+    filename = "runtime.log",
+    filemode = "w",
+    format = "%(asctime)s %(levelname)s: %(message)s"
+)
+
 app = Flask(__name__)
 app.register_blueprint(iot_receive_app)
+
+def initialize():
+    tmp_folder_path = os.path.join(os.getcwd(), "tmp")
+    if os.path.exists(tmp_folder_path):
+        shutil.rmtree(tmp_folder_path)
+    os.makedirs(tmp_folder_path)
 
 @app.route("/", methods=["GET"])
 def hello_world():
@@ -24,16 +40,17 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    print(f"Request body: {body}")
+    logging.debug(f"Request body: {body}")
 
     # handle webhook body
     try:
         line_webhook.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
+        logging.error("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return "OK"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    initialize()
+    app.run(host="0.0.0.0", port=8080, debug=True)
